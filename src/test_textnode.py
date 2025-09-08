1c2c69912917e74b074dd503cb915aec9429d423
justin <8211769+Justikun@users.noreply.github.com>
2025-09-08 02:32:12 -0600

    chore: update tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,14 +9,27 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
-    def test_not_eq(self):
+    def test_eq_false(self):
         node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.LINK)
+        node2 = TextNode("This", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
-    def test_url_none(self):
-        node = TextNode("This is a text node", TextType.BOLD, None)
-        self.assertIsNone(node.url)
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.LINK, "https://example.com")
+        node2 = TextNode("This is a text node", TextType.LINK, "https://example.com")
+        self.assertEqual(node, node2)
+
+    def test_rep(self):
+        node = TextNode("TextNode", TextType.TEXT, "https://example.com")
+        self.assertEqual(
+            "TextNode(TextNode, text, https://example.com)", repr(node)
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
