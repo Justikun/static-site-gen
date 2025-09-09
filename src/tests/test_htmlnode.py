ca56b8a01ac4a384b37fa86f3dad6721a8650de5
justin <8211769+Justikun@users.noreply.github.com>
2025-09-08 19:01:58 -0600

    refactor: refactor folder layout. Add htmlnode tests

--- src/tests/test_htmlnode.py
+++ src/tests/test_htmlnode.py
@@ -3,3 +3,23 @@ import unittest
 from src.HTMLNode import HTMLNode
 
 class test_htmlnode(unittest.TestCase):
+
+    def test_props_to_html(self):
+        node = HTMLNode("p", "This is a node with props", props={"href": "https://example.com"})
+        self.assertEqual(node.props, {"href": "https://example.com"})
+
+    def test_props2_to_html(self):
+        node = HTMLNode("p",
+                        "This is a node with props",
+                        props={
+                            "href": "https://example.com",
+                            "target": "_blank"},
+                        )
+        self.assertEqual(node.props, {"href": "https://example.com", "target": "_blank"})
+
+    def test_defaults_are_empty(self):
+        node = HTMLNode()
+        assert node.tag is None
+        assert node.value is None
+        assert node.children == []
+        assert node.props == {}
