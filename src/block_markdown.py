110813c87cbadde75344f0e0f899b388704a5c6a
justin <8211769+Justikun@users.noreply.github.com>
2025-09-12 01:19:40 -0600

    add block to block_typ checking

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,71 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+def block_to_block_type(block):
+    """
+    :param markdown_block: string
+    :return: BlockType
+    """
+
+    # single line block
+    print(block)
+    if block[:2] == '# ':
+        return BlockType.HEADING
+    if block[:3] == '## ':
+        return BlockType.HEADING
+    if block[:4] == '### ':
+        return BlockType.HEADING
+    if block[:5] == '#### ':
+        return BlockType.HEADING
+    if block[:6] == '##### ':
+        return BlockType.HEADING
+    if block[:7] == '###### ':
+        return BlockType.HEADING
+    if block[:3] and block[len(block)-3:] == '```':
+        return BlockType.CODE
+
+    # multiline line block
+    if block[:2] == "- ":
+        blocks = block.split("\n")
+        for block in blocks:
+            if block[:2] == "- ":
+                continue
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    elif block[:2] == "> ":
+        blocks = block.split("\n")
+        for block in blocks:
+            if block[:2] == "> ":
+                continue
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+
+    pattern = r"\d+\.\s.*"
+    match = re.search(pattern, block)
+    if match:
+        blocks = block.split("\n")
+        for block in blocks:
+            re.search(pattern, block)
+            if match:
+                continue
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
+
+
+
 def markdown_to_block(markdown):
     """
     :param markdown: string representing an entire markdown segment
@@ -14,10 +82,9 @@ def markdown_to_block(markdown):
     return new_blocks
 
 
-
 def main():
     md = """
-This is **bolded** paragraph
+#This is **bolded** paragraph
 
 This is another paragraph with _italic_ text and `code` here
 This is the same paragraph on a new line
@@ -25,11 +92,11 @@ This is the same paragraph on a new line
 - This is a list
 - with items
 """
-    markdown_to_block(md)
 
+    blocks = markdown_to_block(md)
 
-
+    text = "2. this is heading text"
+    print(block_to_block_type(text))
 
 if __name__ == "__main__":
     main()
-
--- src/tests/test_block_markdown.py
+++ src/tests/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.block_markdown import markdown_to_block
+from src.block_markdown import markdown_to_block, block_to_block_type, BlockType
 
 class TestMDToBlock(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -23,3 +23,35 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+class TestBlockToBlockType(unittest.TestCase):
+
+    def test_paragraph(self):
+        text = "This is a normal paragraph"
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_heading1(self):
+        text = "# heading 1"
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_heading2(self):
+        text = "## heading 1"
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_heading6(self):
+        text = "###### heading 6"
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_ordered(self):
+        text = "1. item tems item\n3. asdl"
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_unordered(self):
+        text = "- unordered. - unordered 2"
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
